be78ebc0b07e434a41a4d8924bf1f7c20df56aa6
zainjanjua <zainjanjua74@gmail.com>
2022-09-04 02:31:40 -0400

    Change in install.sh File

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
-#!bin/bash
+#!bash
 
-
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-arm64
-sudo install minikube-linux-arm64 /usr/local/bin/minikube
-minikube start
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl 
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo apt-get update && \
+    sudo apt-get install docker.io -y
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+ minikube version
 
